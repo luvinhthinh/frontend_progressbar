46b0b15f03861253a7472e6069ccb820e44c5c1b
luvinhthinh <luvinhthinh.mono@gmail.com>
2015-11-29 13:30:02 +0800

    Add test runner

---
+++ test/features/step_definitions/Runner.java
@@ -0,0 +1,9 @@
+package step_definitions;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="features")
+public class Runner {
+}
