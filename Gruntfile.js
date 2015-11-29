8fc0160ea41cdddc65a34297a7c7ddb1433b4700
luvinhthinh <luvinhthinh.mono@gmail.com>
2015-11-29 13:17:13 +0800

    Modify pakage.json and gruntfile
    Add clean and mifified task

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,35 @@ module.exports = function (grunt) {
 
     require('load-grunt-tasks')(grunt);
 
+    var SRC_ITEMS = [
+        'public/js/data.js',
+        'public/js/helpers.js',
+        'public/js/myProgressBar.js'
+    ];
+
     grunt.initConfig({
+        app:{
+            dist : 'public/js/dist'
+        },
+        pkg: grunt.file.readJSON('package.json'),
+        clean: {
+            dist: {
+                files: [
+                    {
+                        dot: true,
+                        src: [
+                            '<%= app.dist %>/*'
+                        ]
+                    }
+                ]
+            }
+        },
+        uglify: {
+            build: {
+                src: SRC_ITEMS,
+                dest: '<%= app.dist %>/<%= pkg.name %>.min.js'
+            }
+        },
         sass: {
             dist: {
                 files: {
@@ -16,4 +44,8 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-sass');
 
 
+    grunt.registerTask('minified', [
+        'clean:dist',
+        'uglify'
+    ]);
 };
