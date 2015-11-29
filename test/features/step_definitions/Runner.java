653d51bd5c2037eb4304113cc0355061cabbfea9
luvinhthinh <luvinhthinh.mono@gmail.com>
2015-11-29 14:06:10 +0800

    Modify build.gradle
    add test task

--- test/features/step_definitions/Runner.java
+++ test/features/step_definitions/Runner.java
@@ -4,6 +4,6 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="features")
+@CucumberOptions(features="test/features")
 public class Runner {
 }
